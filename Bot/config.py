43e88edcff64b463122802c69b036c697adfa2fd
Ashu Yadav <90438479+AshuYadavYT@users.noreply.github.com>
2021-09-12 18:10:01 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -5,3 +5,7 @@ colour = 0xf1c40f #color of embeds for bot.
 owner = 804273884966682674 #owners id
 
 admin_role = 804926508677333053 #the role ID of server Admin
+
+join_channel =                   #channel ID where join message will send. Remember your bot must have on privileged gateway intents
+
+join_message = '''You are welcome to our server''' #type your welcome message that bot will send when someone join.

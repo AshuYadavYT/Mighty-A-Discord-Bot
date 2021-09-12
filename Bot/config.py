092e12297c1b22ec49b3ef3ac45746b5adc2536f
Ashu Yadav <90438479+AshuYadavYT@users.noreply.github.com>
2021-09-12 18:00:58 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -4,4 +4,4 @@ colour = 0xf1c40f #color of embeds for bot.
 
 owner = 804273884966682674 #owners id
 
-moderator_role = 804926508677333053 #the role ID of server mod
+admin_role = 804926508677333053 #the role ID of server Admin

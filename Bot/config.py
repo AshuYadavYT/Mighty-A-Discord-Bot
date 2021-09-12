9d8ba1db0efb68ffed833b5c5990d08e520b93a4
Ashu Yadav <90438479+AshuYadavYT@users.noreply.github.com>
2021-09-12 17:57:33 +0530

    Create config.py

---
+++ Bot/config.py
@@ -0,0 +1,7 @@
+prefix = "bot prefix here" #replace your bot token here.
+
+colour = 0xf1c40f #color of embeds for bot.
+
+owner = 804273884966682674 #owners id
+
+moderator_role = 804926508677333053 #the role ID of server mod

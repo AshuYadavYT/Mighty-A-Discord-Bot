b309fad26fe74a5d6654d19d7088129cb6f1ed50
Ashu Yadav <90438479+AshuYadavYT@users.noreply.github.com>
2021-09-15 18:41:51 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -6,6 +6,6 @@ owner = 804273884966682674 #owners id
 
 admin_role = 804926508677333053 #the role ID of server Admin
 
-join_channel =                   #channel ID where join message will send. Remember your bot must have on privileged gateway intents
+join_channel = 804926508677333053 #channel ID where join message will send. Remember your bot must have on privileged gateway intents
 
 join_message = '''You are welcome to our server''' #type your welcome message that bot will send when someone join.
